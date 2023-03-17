1c8594823f946beab0f5e063d8021c86edfd30f5
Adrian Grünberg <adriang2000@gmx.de>
2023-03-17 19:35:49 +0100

    add fetch all button

--- src/components/Path.ControlCenter.js
+++ src/components/Path.ControlCenter.js
@@ -1,12 +1,21 @@
 import { h } from "preact";
 import WebsocketControlPanel from "./Gui.WebsocketControlPanel";
-import FullPage from "./Layout.FullPage"
+import FullPage from "./Layout.FullPage";
+import IDownload from "./icons/IDownload.svg";
 
 export default function ControlCenter(props){
     return (
         <FullPage>
             <h2 class="is-stack-40">Control Center</h2>
             <div class="is-col">
+                <div class="is-row is-stack-40">
+                    <div class="is-col">
+                        <button class="button is-secondary pad-icon">
+                            <IDownload />
+                            Fetch All
+                        </button>
+                    </div>
+                </div>
                 <WebsocketControlPanel />
             </div>
         </FullPage>
